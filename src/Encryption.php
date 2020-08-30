e32225e1404299a7737ee9d8a0fdaa4e9628c5b1
Elys Marcos <elvys.marcos@iatec.com>
2020-08-30 19:59:18 -0300

    Remove get iat

--- src/Encryption.php
+++ src/Encryption.php
@@ -14,14 +14,14 @@ class Encryption
         return json_decode(Encryption::Base64UrlDecode(strrev(Encryption::Base64UrlDecode($data))), true);
     }
 
-    static function Base64UrlEncode(string $data): string
+    static function Base64UrlEncode(string $data)
     {
         $urlSafeData = strtr(base64_encode($data), '+/', '-_');
 
         return rtrim($urlSafeData, '=');
     }
 
-    static function Base64UrlDecode(string $data): array
+    static function Base64UrlDecode(string $data)
     {
         $urlUnsafeData = strtr($data, '-_', '+/');
 
