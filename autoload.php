cf1a7562093cc852f34c7bdc51ffbdc2eaf3ed71
Elys Marcos <elvys.marcos@iatec.com>
2020-10-13 07:39:19 -0300

    Fixs class for load path settings from env configs

--- autoload.php
+++ autoload.php
@@ -2,11 +2,11 @@
 require_once 'vendor/autoload.php';
 
 spl_autoload_register(
-	function ($class) {
-		$file = str_replace("\\", "/", $class . '.php');
+    function ($class) {
+        $file = str_replace("\\", "/", $class . '.php');
 
-		if (file_exists(PATH_ROOT . $file)) {
-			include_once(PATH_ROOT . $file);
-		}
-	}
+        if (file_exists($_ENV['PATH_ROOT'] . $file)) {
+            include_once($_ENV['PATH_ROOT'] . $file);
+        }
+    }
 );
